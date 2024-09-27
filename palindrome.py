5c8523457923919d95d0d115c0d3b2b3c62a1683
Krithikk Thilak <krithikkthilak18@gmail.com>
2024-09-27 15:15:15 +0530

    Update palindrome.py
    krithikk da

--- palindrome.py
+++ palindrome.py
@@ -1,1 +1,12 @@
+a=int(input("enter the number:"))
+res=0
+check=a
+while a!=0:
+    rem = a%10
+    res=(res*10)+rem
+    a=a//10
 
+if check==res:
+    print(res, "it is a palindrom")
+else:
+    print(res,"is not a palindrom")
