27f05e930f3255350dfa6f7a8622363f12f8c56c
Krithikk Thilak <krithikkthilak18@gmail.com>
2024-09-29 15:27:21 +0530

    Add files via upload
    mapla I used little help of youtube and your code, enaku target not found varala couldn't found why paathu sollu

--- linearsearch.py
+++ linearsearch.py
@@ -1,20 +1,23 @@
-n=int(input("Enter number of elements in array: "))
-l=[ ]
-found=0
-pos=0
-
+from array import *
+arr1 = array('i',[])
+k=0
+n = int(input("enter the size of your array: "))
 for i in range(0,n):
-    val=int(input("Enter element: "))
-    l.append(val)
-
-tar=int(input("Enter target element: "))
-
+    s=int(input("enter the values: "))
+    arr1.append(s)
+num=int(input("enter the number you want to find: "))
+target=num
 for i in range(0,n):
-    if l[i]==tar:
+    if arr1[i]==target:
         found=1
-        pos=i
-
+        c=i
+    
 if found==1:
-    print("Element found @ ",pos," index.")
+    print("element found in ",c+1,)
 else:
-    print("Element not found.")
+    print("target not found")
+
+
+
+
+
