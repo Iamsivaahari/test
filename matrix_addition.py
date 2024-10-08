79e1e18fcf94b8a23818465f5ea5df124832fe53
Iamsivaahari <ssharicharan2005@gmail.com>
2024-10-08 12:09:16 +0530

    Update matrix_addition.py

--- matrix_addition.py
+++ matrix_addition.py
@@ -4,51 +4,52 @@ print(" ")
 row2=int(input("Enter number of ROWS in MATRIX 2: "))
 col2=int(input("Enter number of COLUMNS in MATRIX 2: "))
 
-mat1=[]
-mat2=[]
-print(" ")
-print("Entering values of MATRIX 1.")
-for i in range(0,row1):
-    temp=[]
-    for j in range(0,col1):
-        val=int(input("Enter value: "))
-        temp.append(val)
-    mat1.append(temp)
+if row1==row2 and col1==col2:
+    mat1=[]
+    mat2=[]
+    print(" ")
+    print("Entering values of MATRIX 1.")
+    for i in range(0,row1):
+        temp=[]
+        for j in range(0,col1):
+            val=int(input("Enter value: "))
+            temp.append(val)
+        mat1.append(temp)
 
-print(" ")
-print("Entering values of MATRIX 2.")
-for i in range(0,row2):
-    temp=[]
-    for j in range(0,col2):
-        val=int(input("Enter value: "))
-        temp.append(val)
-    mat2.append(temp)
+    print(" ")
+    print("Entering values of MATRIX 2.")
+    for i in range(0,row2):
+        temp=[]
+        for j in range(0,col2):
+            val=int(input("Enter value: "))
+            temp.append(val)
+        mat2.append(temp)
 
-print(" ")
-print("MATRIX 1: ")
-for i in range(0,row1):
-    te=mat1[i]
-    print(te) 
+    print(" ")
+    print("MATRIX 1: ")
+    for i in range(0,row1):
+        te=mat1[i]
+        print(te) 
 
-print(" ")
-print("MATRIX 2: ")
-for i in range(0,row2):
-    te=mat1[i]
-    print(te)        
+    print(" ")
+    print("MATRIX 2: ")
+    for i in range(0,row2):
+        te=mat2[i]
+        print(te)        
 
-add=[]
-print(" ")
-print("MATRIX ADDITION: ")
-for i in range(0,row1):
-    tes=[]
-    for j in range(0,col1):
-        sum=mat1[i][j]+mat2[i][j]
-        tes.append(sum)
-    add.append(tes)
+    add=[]
+    print(" ")
+    print("MATRIX ADDITION: ")
+    for i in range(0,row1):
+        tes=[]
+        for j in range(0,col1):
+            sum=mat1[i][j]+mat2[i][j]
+            tes.append(sum)
+        add.append(tes)
 
-print(" ")
-print(" ")
-print("RESULTANT MATRIX: ")
-for i in range(0,row1):
-    te=add[i]
-    print(te)
+    print(" ")
+    print(" ")
+    print("RESULTANT MATRIX: ")
+    for i in range(0,row1):
+        te=add[i]
+        print(te)
